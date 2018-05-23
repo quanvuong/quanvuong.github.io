47562d7723ea4939c4bed3719b6eeb9ef01502d6
Quan Vuong <quan.vuong@nyu.edu>
2018-05-23 11:41:14 +0300

    put BasicInfo and Photo into the same component

--- src/App.js
+++ src/App.js
@@ -8,8 +8,10 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Photo/>
-        <BasicInfo/>
+        <div className="Header">
+          <Photo/>
+          <BasicInfo/>
+        </div>
       </div>
     );
   }
