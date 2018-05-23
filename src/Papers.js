d229ee5dd04f86689af1bdc656df94f3e498d15d
Quan Vuong <quan.vuong@nyu.edu>
2018-05-23 11:44:03 +0300

    add empty Papers component

---
+++ src/Papers.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Papers extends Component {
+  render() {
+    return (
+      <div className="Papers">
+      </div>
+    );
+  }
+}
+
+export default Papers;
