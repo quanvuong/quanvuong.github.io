f03f12b3d0f6467dacaa1216b086decf84d04473
Quan Vuong <quan.vuong@nyu.edu>
2018-09-19 12:32:34 -0700

    add text for summary

--- src/Summary.js
+++ src/Summary.js
@@ -4,7 +4,9 @@ class Summary extends Component {
   render() {
     return (
       <div className="Summary">
-        <div>Summary Text</div>
+        <div>Hello! I am Quan Vuong, currently a PhD student in the Artificial Intelligence group at the University of California San Diego. My advisor is Professor Ndapa Nakashole. We work on Natural Language Processing tasks.</div>
+
+        <div>In my undergraduate, I had the pleasure of working with Professor Keith Ross on Reinforcement Learning and Professor Godfried Toussaint on Computational Geometry.</div>
       </div>
     );
   }
