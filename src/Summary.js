df37ca734b52a22f0b75b6a3a53537fd9b560e31
Quan Vuong <quan.vuong@nyu.edu>
2018-09-24 17:46:24 -0700

    shorten undergrad

--- src/Summary.js
+++ src/Summary.js
@@ -6,7 +6,7 @@ class Summary extends Component {
       <div className="Summary">
         <div>Hello! I am Quan Vuong, currently a PhD student in the Artificial Intelligence group at the University of California San Diego. My advisor is Professor Ndapa Nakashole. We work on Natural Language Processing tasks.</div>
 
-        <div>In my undergraduate, I worked with Professor Keith Ross on Reinforcement Learning and Professor Godfried Toussaint on Computational Geometry. My team also won the UAE's national innovation competition for university students, worth 250000 USD. I also served as the Student Body Vice President.</div>
+        <div>In undergrad, I worked with Professor Keith Ross on Reinforcement Learning and Professor Godfried Toussaint on Computational Geometry. My team also won the UAE's national innovation competition for university students, worth 250000 USD. I also served as the Student Body Vice President.</div>
       </div>
     );
   }
