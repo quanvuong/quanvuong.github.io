4f90af4e692a310e17621be4ff90d5b9b830924d
Quan Vuong <quan.vuong@nyu.edu>
2018-09-24 18:37:25 -0700

    complete website

--- src/Papers.js
+++ src/Papers.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
+import "./Papers.css";
 
 class Papers extends Component {
   render() {
-    let entropy = ["Efficient Entropy For Policy Gradient with Multi-Dimensional Action Space.", "https://arxiv.org/abs/1806.00589", "A short version appears in ICLR 2018 Workshop", "Yiming Zhang, Quan Vuong, Kenny Song, Xiao-Yue Gong, Keith W. Ross"]
+    let entropy = ["Efficient Entropy For Policy Gradient with Multi-Dimensional Action Space.", "https://arxiv.org/abs/1806.00589", "A short version appeared in ICLR 2018 Workshop", "Yiming Zhang, Quan Vuong, Kenny Song, Xiao-Yue Gong, Keith W. Ross"]
 
     let img_complexity = ["Measuring the complexity of two-dimensional binary patterns — Sub-symmetries versus Papentin complexity.", "https://ieeexplore.ieee.org/document/7153235/", "IAPR International Conference on Machine Vision Applications 2015", "Godfried Toussaint, Noris Onea, Quan Vuong"]
 
@@ -12,27 +13,33 @@ class Papers extends Component {
 
     return (
       <div className="Papers">
-        <div>Papers</div>
+        <div className="PaperHeader">Papers</div>
 
         <ul>
           {papers.map(p => {
-            return <li className="OnePaper">
-              <a href={p[1]}>{p[0]}</a>
-              <div>{p[2]}</div>
-              <div>{p[3]}</div>
+            return <li>
+              <div className="OnePaper">
+                <a href={p[1]}>{p[0]}</a>
+                <div>{p[2]}</div>
+                <div>{p[3]}</div>
+              </div>
             </li>
           })}
 
-          <li className="OnePaper">
-            <div><a href={entropy[1]}>{entropy[0]}</a></div>
-            <div><a href="https://openreview.net/pdf?id=rJc8sN1vG">{entropy[2]}</a></div>
-            <div>{entropy[3]}</div>
+          <li>
+            <div className="OnePaper">
+              <div><a href={entropy[1]}>{entropy[0]}</a></div>
+              <div><a href="https://openreview.net/pdf?id=rJc8sN1vG">{entropy[2]}</a></div>
+              <div>{entropy[3]}</div>
+            </div>
           </li>
 
-          <li className="OnePaper">
-            <div><a href={img_complexity[1]}>{img_complexity[0]}</a></div>
-            <div>{entropy[2]}</div>
-            <div>{entropy[3]}</div>
+          <li>
+            <div className="OnePaper">
+              <div><a href={img_complexity[1]}>{img_complexity[0]}</a></div>
+              <div>{entropy[2]}</div>
+              <div>{entropy[3]}</div>
+            </div>
           </li>
 
         </ul>
--- src/Summary.js
+++ src/Summary.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
+import './Summary.css';
 
 class Summary extends Component {
   render() {
     return (
       <div className="Summary">
-        <div>Hello! I am Quan Vuong, currently a PhD student in the Artificial Intelligence group at the University of California San Diego. My advisor is Professor Ndapa Nakashole. We work on developing sample-efficient learning approach to Natural Language Processing tasks.</div>
+        <div className="SummaryOneP">Hello! I am Quan Vuong, currently a PhD student in the <a href="http://ai.ucsd.edu/">Artificial Intelligence</a> group at the University of California San Diego. My advisor is Professor <a href="http://nakashole.com/">Ndapa Nakashole</a>. We work on developing sample-efficient learning approach to Natural Language Processing tasks.</div>
 
-        <div>In undergrad, I worked with Professor Keith Ross on Reinforcement Learning and Professor Godfried Toussaint on Computational Geometry. My team also won the UAE's national innovation competition for university students, worth 250000 USD. I also served as the Student Body Vice President.</div>
+        <div className="SummaryOneP">In undergrad, I worked with Professor <a href="http://www.nyu.edu/projects/keithwross/">Keith Ross</a> on Reinforcement Learning and Professor <a href="http://cgm.cs.mcgill.ca/~godfried/">Godfried Toussaint</a> on Computational Geometry. My team won the UAE's national <a href="https://nyuad.nyu.edu/en/news/latest-news/honors-and-awards/2016/february/roadwatch-app-wins-uae-best-m-government-service-award.html">innovation competition</a> for university students, worth 250000 USD. I also served as the Student Body Vice President.</div>
       </div>
     );
   }
