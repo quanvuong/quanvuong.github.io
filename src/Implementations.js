2af93739c6b1c37e83e1cecd78d50879c4629596
Quan Vuong <quan.vuong@nyu.edu>
2019-02-07 22:32:48 -0800

    paraphase the miscellaneous section

--- src/Implementations.js
+++ src/Implementations.js
@@ -12,16 +12,14 @@ class Implementations extends Component {
 
           <li>
             <div className="OnePaper">
-              <div>Deep Reinforcement Learning in a Handful of Trials using Probabilistic Dynamics Models</div>
-              <div>Chua et al.</div>
+              <div>Pytorch implementation of "Deep Reinforcement Learning in a Handful of Trials using Probabilistic Dynamics Models" by Chua et al.</div>
               <div> <a href="https://github.com/quanvuong/handful-of-trials-pytorch">Code</a> </div>
             </div>
           </li>
 
           <li>
             <div className="OnePaper">
-              <div>Algorithmic Framework for Model-based Deep Reinforcement Learning with Theoretical Guarantees</div>
-              <div>Luo et al.</div>
+              <div>Proofs for "Algorithmic Framework for Model-based Deep Reinforcement Learning with Theoretical Guarantees" by Luo et al.</div>
               <div>To understand this paper, I provided detailed proofs of its theoretical results. <a href="https://drive.google.com/file/d/1KftRUTW2jP8XIB3cwSBgh7xvveDjKDcn/view?usp=sharing">Proofs</a> </div>
             </div>
           </li>
