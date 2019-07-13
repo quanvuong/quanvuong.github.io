2169260b8b3f79ab2c628df5ab9a34a015cf5385
Quan Vuong <quan.vuong@nyu.edu>
2019-07-13 11:56:33 +0100

    remove AI group UCSD from info

--- src/BasicInfo.js
+++ src/BasicInfo.js
@@ -7,7 +7,7 @@ class BasicInfo extends Component {
       <div className="BasicInfo">
         <div>Quan Vuong</div><br/>
         <div><a href="mailto:quan.hovuong@gmail.com">quan.hovuong@gmail.com</a></div><br/>
-        <div>First-year PhD Student, UC San Diego <a href="http://ai.ucsd.edu/">Artificial Intelligence Group</a></div><br/>
+        <div>First-year PhD Student, UC San Diego </div><br/>
         <div>Fields: Deep Reinforcement Learning, Robotics</div><br/>
         <div><a href="https://drive.google.com/file/d/1DpEitRs4Vk8bhtCneUNOYMxa2f2Bai-m/view?usp=sharing">Curriculum Vitae</a></div><br/>
       </div>
