c457d954c5ed98c638bdf19e057cea09634cb3be
Quan Vuong <quan.vuong@nyu.edu>
2019-08-08 15:31:05 +0700

    change to second year student

--- src/BasicInfo.js
+++ src/BasicInfo.js
@@ -5,11 +5,11 @@ class BasicInfo extends Component {
   render() {
     return (
       <div className="BasicInfo">
-        <div>Quan Vuong</div><br/>
-        <div><a href="mailto:quan.hovuong@gmail.com">quan.hovuong@gmail.com</a></div><br/>
-        <div>First-year PhD Student, UC San Diego </div><br/>
-        <div>Fields: Deep Reinforcement Learning, Robotics</div><br/>
-        <div><a href="https://drive.google.com/file/d/1DpEitRs4Vk8bhtCneUNOYMxa2f2Bai-m/view?usp=sharing">Curriculum Vitae</a></div><br/>
+        <div>Quan Vuong</div><br />
+        <div><a href="mailto:quan.hovuong@gmail.com">quan.hovuong@gmail.com</a></div><br />
+        <div>Second-year PhD Student, UC San Diego </div><br />
+        <div>Fields: Deep Reinforcement Learning, Robotics</div><br />
+        <div><a href="https://drive.google.com/file/d/1DpEitRs4Vk8bhtCneUNOYMxa2f2Bai-m/view?usp=sharing">Curriculum Vitae</a></div><br />
       </div>
     );
   }
