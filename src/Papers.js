8b9c16d7887d2cd6f7ea8f31e5bfe2146d222c32
Quan Vuong <quan.vuong@nyu.edu>
2019-09-30 20:48:18 -0700

    remove dot

--- src/Papers.js
+++ src/Papers.js
@@ -3,11 +3,11 @@ import "./Papers.css";
 
 class Papers extends Component {
   render() {
-    let entropy = ["Efficient Entropy For Policy Gradient with Multi-Dimensional Action Space.", "https://arxiv.org/abs/1806.00589", "A short version appeared in ICLR 2018 Workshop"]
+    let entropy = ["Efficient Entropy For Policy Gradient with Multi-Dimensional Action Space", "https://arxiv.org/abs/1806.00589", "A short version appeared in ICLR 2018 Workshop"]
 
-    let img_complexity = ["Measuring the complexity of two-dimensional binary patterns — Sub-symmetries versus Papentin complexity.", "https://ieeexplore.ieee.org/document/7153235/", "IAPR International Conference on Machine Vision Applications 2015"]
+    let img_complexity = ["Measuring the complexity of two-dimensional binary patterns — Sub-symmetries versus Papentin complexity", "https://ieeexplore.ieee.org/document/7153235/", "IAPR International Conference on Machine Vision Applications 2015"]
 
-    let spu = ["Supervised Policy Update for Deep Reinforcement Learning.", "https://arxiv.org/abs/1805.11706"]
+    let spu = ["Supervised Policy Update for Deep Reinforcement Learning", "https://arxiv.org/abs/1805.11706"]
 
     return (
       <div className="Papers">
@@ -51,7 +51,7 @@ class Papers extends Component {
             <li>
               <div className="OnePaper">
                 <div><strong>{spu[0]}</strong></div>
-                <div>International Conference on Learning Representations <strong>(ICLR)</strong> 2019. <a href={spu[1]}>PDF</a> <a href="https://drive.google.com/file/d/1Yg8x4bSzoj1t6Sk-_N3jTx2eTaVZGoqW/view?usp=sharing">Slide</a> <a href="https://drive.google.com/open?id=1lJeP_3YNlUoMaU-RDENDrkfjPdZQYpA3">Poster</a> <a href="https://github.com/quanvuong/Supervised_Policy_Update">Code</a>
+                <div>International Conference on Learning Representations <strong>(ICLR)</strong> 2019 <a href={spu[1]}>PDF</a> <a href="https://drive.google.com/file/d/1Yg8x4bSzoj1t6Sk-_N3jTx2eTaVZGoqW/view?usp=sharing">Slide</a> <a href="https://drive.google.com/open?id=1lJeP_3YNlUoMaU-RDENDrkfjPdZQYpA3">Poster</a> <a href="https://github.com/quanvuong/Supervised_Policy_Update">Code</a>
                 </div>
                 <div> <strong>Vuong</strong>, Zhang, Ross </div>
               </div>
