6f829b2c786409613e02f4ae9d35bb243dc2808b
Quan Vuong <quan.vuong@nyu.edu>
2019-10-01 23:00:11 -0700

    remove {} in implementations

--- src/Implementations.js
+++ src/Implementations.js
@@ -10,11 +10,11 @@ class Implementations extends Component {
 
         <ol>
 
-        <li>
-          <div className="OnePaper">
-            <div> <a href="https://github.com/quanvuong/paper_summaries">Paper Summaries</a> </div>
-          </div>
-        </li>
+          <li>
+            <div className="OnePaper">
+              <div> <a href="https://github.com/quanvuong/paper_summaries">Paper Summaries</a> </div>
+            </div>
+          </li>
 
           <li>
             <div className="OnePaper">
@@ -22,24 +22,19 @@ class Implementations extends Component {
             </div>
           </li>
 
-                    {
-
           <li>
             <div className="OnePaper">
               <div><a href="https://github.com/quanvuong/Advanced-Basic-Theoretical-Reinforcement-Learning-Result">Advanced Basic Theoretical Reinforcement Learning Result</a> </div>
             </div>
           </li>
-          }
-
-          {
 
           <li>
-          <div className="OnePaper">
-            <div>I often found myself forgetting knowledge. I thus created self-test. I do them at fixed interval to keep basic results in my brain.</div>
-            <div><a href="https://drive.google.com/open?id=1xg4gVeZNUsOijDgens9VWjOwMAVCPlNP">Self-tests </a> (Always In Progress)</div>
-          </div>
-        </li>
-          }
+            <div className="OnePaper">
+              <div>I often found myself forgetting knowledge. I thus created self-test. I do them at fixed interval to keep basic results in my brain.</div>
+              <div><a href="https://drive.google.com/open?id=1xg4gVeZNUsOijDgens9VWjOwMAVCPlNP">Self-tests </a> (Always In Progress)</div>
+            </div>
+          </li>
+
         </ol>
 
       </div>
