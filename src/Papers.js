a6bd65238c523475df70987ca8055c1448ceb925
Quan Vuong <quan.vuong@nyu.edu>
2019-10-08 08:55:23 -0700

    add pdf for sop

--- src/Papers.js
+++ src/Papers.js
@@ -44,7 +44,7 @@ class Papers extends Component {
             <li>
               <div className="OnePaper">
                 <div>Towards Simplicity in Deep Reinforcement Learning: Streamlined Off-Policy Learning</div>
-                <div>Preprint. </div>
+                <div>Preprint. <a href="https://arxiv.org/abs/1910.02208">PDF</a> </div>
                 <div> Wang, Wu, <strong>Vuong</strong> , Ross </div>
               </div>
             </li>
