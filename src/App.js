dbe3d8e7034b5af9d922c7bb1fe84b1055d5ea4c
Quan Vuong <quan.vuong@nyu.edu>
2019-12-08 23:57:54 -0800

    add oac to media

--- src/App.js
+++ src/App.js
@@ -6,19 +6,21 @@ import BasicInfo from './BasicInfo.js';
 import Summary from './Summary.js';
 import Papers from './Papers.js';
 import Implementaions from './Implementations.js';
+import Media from './Media.js';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <div className="Header">
-          <Photo/>
-          <BasicInfo/>
+          <Photo />
+          <BasicInfo />
         </div>
         <div className="Body">
-          <Summary/>
-          <Papers/>
-          <Implementaions/>
+          <Summary />
+          <Papers />
+          <Media />
+          <Implementaions />
         </div>
       </div>
     );
