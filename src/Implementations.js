c07a68328228032a199fffc0e9422e957d1394c7
Quan Vuong <quan.vuong@nyu.edu>
2020-02-01 10:44:41 -0800

    remove forgetting

--- src/Implementations.js
+++ src/Implementations.js
@@ -40,12 +40,12 @@ class Implementations extends Component {
             </div>
           </li>
 
-          <li>
+          {/* <li>
             <div className="OnePaper">
               <div>I often found myself forgetting knowledge. I thus created self-test. I do them at fixed interval to keep basic results in my brain.</div>
               <div><a href="https://drive.google.com/open?id=1xg4gVeZNUsOijDgens9VWjOwMAVCPlNP">Self-tests </a> (Always In Progress)</div>
             </div>
-          </li>
+          </li>*/}
 
         </ol>
 
