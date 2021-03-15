f57f4002989e3bf0ade214ec717ebe2eb7c85818
Ubuntu 18 desktop <quan.hovuong@gmail.com>
2021-03-14 19:39:16 -0700

    ck ftw

--- src/App.js
+++ src/App.js
@@ -11,14 +11,36 @@ import Award from './Award.js';
 import funnyPhoto from './6d94c45d2cafa385a8be5f3ea8.jpg';
 
 class App extends Component {
+
   render() {
+
+    const fun_stuff = [
+      {
+        img_src: "https://img.cryptokitties.co/0x06012c8cf97bead5deae237070f9587f8e7a266d/250.png",
+        img_cre: "https://www.cryptokitties.co/kitty/250"
+      },
+      {
+        img_src: funnyPhoto,
+        img_cre: "http://www.jimbenton.com/"
+      },
+      {
+        img_src: "http://www.jimbenton.com/page5/page27/files/Jim%20Benton%202-2-13.jpg",
+        img_cre: "http://www.jimbenton.com/"
+      }
+    ]
+
+    const len = fun_stuff.length;
+
+    let fun = fun_stuff[Math.floor(Math.random() * len)];
+
     return (
       <div className="App">
         <div className="Header">
           <Photo />
           <BasicInfo />
           <div className="Photo">
-            <img src={funnyPhoto} width='250' alt='' />
+            <img src={fun.img_src} width='250' alt='' />
+            <div><a href={fun.img_cre}>imgcred</a></div>
           </div>
         </div>
         <div className="Body">
